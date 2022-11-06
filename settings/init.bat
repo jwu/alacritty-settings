d1984a19865c22a20da93f595a6312bb8aa3391d
Johnny Wu <wujie@xmfunny.com>
2022-11-06 09:15:59 +0800

    better code

--- settings/init.bat
+++ settings/init.bat
@@ -6,8 +6,7 @@
 set __INIT_START__=%time%
 
 :: debug config
-set DBG_VAR=0
-set DBG_INIT_TIME=0
+set __DBG_INFO__=0
 
 :: cmd config
 set LANG=en_US.utf8
@@ -33,7 +32,7 @@ call "%AL_SETTINGS%\cmds\aliases.cmd"
 :: ========================================
 
 if exist "%AL_VENDOR%\git_portable" (
-  set "GIT_ROOT=%AL_VENDOR%\git_portable"
+  set "AL_GIT_ROOT=%AL_VENDOR%\git_portable"
   goto :CONFIG_GIT
 ) else (
   goto :NO_GIT
@@ -42,20 +41,20 @@ if exist "%AL_VENDOR%\git_portable" (
 :CONFIG_GIT
 
 :: add git to environment path
-if exist "%GIT_ROOT%\cmd\git.exe" (
-  set "PATH=%PATH%;%GIT_ROOT%\cmd"
+if exist "%AL_GIT_ROOT%\cmd\git.exe" (
+  set "PATH=%PATH%;%AL_GIT_ROOT%\cmd"
 )
 
 :: add usr\bin to environment path
-if exist "%GIT_ROOT%\usr\bin" (
-  set "PATH=%PATH%;%GIT_ROOT%\usr\bin"
+if exist "%AL_GIT_ROOT%\usr\bin" (
+  set "PATH=%PATH%;%AL_GIT_ROOT%\usr\bin"
 )
 
 :: add mingw to environment path
-if exist "%GIT_ROOT%\mingw32" (
-  set "PATH=%PATH%;%GIT_ROOT%\mingw32\bin"
-) else if exist "%GIT_ROOT%\mingw64" (
-  set "PATH=%PATH%;%GIT_ROOT%\mingw64\bin"
+if exist "%AL_GIT_ROOT%\mingw32" (
+  set "PATH=%PATH%;%AL_GIT_ROOT%\mingw32\bin"
+) else if exist "%AL_GIT_ROOT%\mingw64" (
+  set "PATH=%PATH%;%AL_GIT_ROOT%\mingw64\bin"
 )
 
 goto :END_CONFIG_GIT
@@ -88,19 +87,16 @@ if "%PROCESSOR_ARCHITECTURE%"=="x86" (
 
 set __INIT_END__=%time%
 
-:: debug variables
-if %DBG_VAR% gtr 0 (
+:: show debug info
+if %__DBG_INFO__% gtr 0 (
+  "%AL_SETTINGS%\cmds\timer.cmd" "%__INIT_START__%" "%__INIT_END__%"
+  echo ----------------------------------------
   echo PROCESSOR_ARCHITECTURE = %PROCESSOR_ARCHITECTURE%
   echo LANG = %LANG%
   echo AL_ROOT = %AL_ROOT%
   echo AL_SETTINGS = %AL_SETTINGS%
   echo AL_VENDOR = %AL_VENDOR%
-  echo GIT_ROOT = %GIT_ROOT%
-)
-
-:: debug init time
-if %DBG_INIT_TIME% gtr 0 (
-  "%AL_SETTINGS%\cmds\timer.cmd" "%__INIT_START__%" "%__INIT_END__%"
+  echo AL_GIT_ROOT = %AL_GIT_ROOT%
 )
 
 exit /b
